64c2da58b1649f6ec3eb8130b50365f0eb87bcb8
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2022-09-03 01:09:02 +0900

    loading&record-changes-finish

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,6 +1,24 @@
 'use strict';
 
 {
+  let $setTime;
+  $('#record-modalButton').on('click',function(){
+    $('#modal-top').css("display","none");
+    $('#loading').css("display","block");
+    $setTime = setTimeout(function(){
+        $('#loading').css("display","none");
+        $('#access-record').css("display","block");
+    },3000);
+  });
+
+
+  $('.js-closeModal').on('click',function(){
+    clearTimeout($setTime);
+    $('#modal-top').css("display","block");
+    $('#loading').css("display","none");
+    $('#access-record').css("display","none");
+  });
+
   const $container = $('.overlay')
   const $button = $('.js-openModal')
   const $closeButton = $('.js-closeModal')
@@ -13,6 +31,5 @@
     $container.removeClass('openModal')
   })
 
-  
-
 }
+
