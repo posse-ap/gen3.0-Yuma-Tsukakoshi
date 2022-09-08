3bfd7bf5b4541f419ce23b58e4fe9adcf3852153
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2022-09-09 02:18:51 +0900

    calendar created

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,22 +1,50 @@
 'use strict';
 
 {
-  let $setTime;
+  let $Timeloading;
   $('#record-modalButton').on('click',function(){
     $('#modal-top').css("display","none");
     $('#loading').css("display","block");
-    $setTime = setTimeout(function(){
+    $Timeloading = setTimeout(function(){
         $('#loading').css("display","none");
         $('#access-record').css("display","block");
     },3000);
   });
 
-
   $('.js-closeModal').on('click',function(){
-    clearTimeout($setTime);
+    clearTimeout($Timeloading);
     $('#modal-top').css("display","block");
     $('#loading').css("display","none");
     $('#access-record').css("display","none");
+  });
+
+  let $TimeCurender;
+  $('#studyDay-modalButton').on('click',function(){
+    $('#modal-top').css("display","none");
+    $('#loading').css("display","block");
+    $TimeCurender = setTimeout(function(){
+        $('#loading').css("display","none");
+        $('#calendar').css("display","block");
+        $('.js-closeModal').css("display","none");
+        $('.modal-back-button').css("display","block");
+    },0);
+  })
+
+  $('.js-closeModal').on('click',function(){
+    clearTimeout($TimeCurender);
+    $('#modal-top').css("display","block");
+    $('#loading').css("display","none");
+    $('#access-record').css("display","none");
+    $('#calendar').css("display","none");
+    $('.modal-back-button').css("display","none");
+    $('.js-closeModal').css("display","block");
+  });
+
+  $('.js-backModal').on('click',function(){
+    $('.js-closeModal').css("display","block");
+    $('#calendar').css("display","none");
+    $('#modal-top').css("display","block");
+    $('.modal-back-button').css("display","none");
   });
 
   const $container = $('.overlay')
@@ -25,12 +53,11 @@
 
   $button.click((e) => {
     $container.addClass('openModal')
+    $('.modal-back-button').css("display","none");
   })
 
   $closeButton.click(() => {
     $container.removeClass('openModal')
   })
-
-  //carender function: showCalender()
 }
 
