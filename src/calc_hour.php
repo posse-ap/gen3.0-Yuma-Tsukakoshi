9633912635ed9afc8edebe7cbecbace1db1a7773
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-01-19 12:55:24 +0900

    success getting hour of each language&content

--- src/calc_hour.php
+++ src/calc_hour.php
@@ -26,7 +26,19 @@ $stmt_total = $pdo->query("SELECT sum(hours) as hour_total FROM study_hours");
 $hour_total = $stmt_total ->fetch(PDO::FETCH_ASSOC);
 
 
-//
+//言語別勉強時間
+$sql_language = "SELECT language_id, languages.language name ,sum(hours) language_hours FROM record_languages INNER JOIN languages ON record_languages.language_id = languages.id GROUP BY language_id ORDER BY language_id";
+$stmt_language = $pdo->prepare($sql_language);
+$stmt_language -> execute();
+$hour_language = $stmt_language->fetchAll(PDO::FETCH_ASSOC);
+
+//コンテンツ別学習時間
+$sql_content = "SELECT content_id,contents.content name, sum(hours) content_hours FROM record_contents INNER JOIN contents ON record_contents.content_id = contents.id GROUP BY content_id ORDER BY content_id";
+$stmt_content = $pdo->prepare($sql_content);
+$stmt_content -> execute();
+$hour_content = $stmt_content->fetchAll(PDO::FETCH_ASSOC);
+
+
 //db構造確認方法：
 // echo "<pre>";
 // print_r($変数名);
