9d74cbdead424b37c307b708af0f210d1899924c
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-10-31 09:40:14 +0900

    add:user dashboardからwebapp画面遷移

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -11,6 +11,9 @@
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
+                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    <a href="{{ route('webapp') }}" class="text-gray-900 underline">WebApp画面へ</a>
+                </button>
             </div>
         </div>
     </div>
