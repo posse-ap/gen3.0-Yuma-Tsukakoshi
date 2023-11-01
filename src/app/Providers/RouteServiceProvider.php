071451f7b7cb705d07b81470288af2ec25e7ac5f
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-01 10:15:46 +0900

    change:ログイン後のurlwebappに変更

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    // public const HOME = '/dashboard';
+    public const HOME = '/webapp';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
