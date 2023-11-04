1d1e7fa6870d9423ba222074a5ca69ac37f1030f
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-04 10:05:23 +0900

    add;content作成ページ作成

---
+++ src/resources/views/admin/contents_create.blade.php
@@ -0,0 +1,38 @@
+<x-app-layout>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            コンテンツの追加
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          @if (session('message'))
+              <div class="text-red-500 font-bold">
+                {{ session('message') }}
+              </div>
+          @endif
+          <form method="post" action="{{route('contents.store')}}">
+            @csrf
+            <div class="mt-8">
+              <div class="w-full flex flex-col">
+                <label for="name" class="font-semibold mt-4">コンテンツ名</label>
+                <input type="text" name="content" class="w-auto py-2 border border-gray-300 rounded-md" id="name">
+              </div>
+            </div>
+
+            <div class="mt-8">
+              <div class="w-full flex flex-col">
+                <label for="color" class="font-semibold mt-4">色</label>
+                <input type="color" name="color" class="w-auto p-8 border border-gray-300 rounded-md" id="color">
+              </div>
+            </div>
+
+            <x-primary-button class="mt-4">
+              送信する
+            </x-primary-button>
+          </form>
+        </div>
+    </div>
+</x-app-layout>
