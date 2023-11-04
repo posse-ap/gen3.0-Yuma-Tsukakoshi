b6511d4352fed0517f873a10a6706e65797ed47e
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-04 09:01:35 +0900

    add;adminseeder追加

---
+++ src/database/seeders/AdminSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('admins')->insert([
+            [
+                'id' => 1,
+                'name' => '管理者1',
+                'email' => 'kakaka@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'id' => 2,
+                'name' => '管理者2',
+                'email' => 'sasasa@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'id' => 3,
+                'name' => '管理者3',
+                'email' => 'yoyoyo@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+        ]);
+    }
+}
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
                 RecordLanguageSeeder::class,
                 StudyHourSeeder::class,
                 UserSeeder::class,
+                AdminSeeder::class,
             ]);
         }
     }
