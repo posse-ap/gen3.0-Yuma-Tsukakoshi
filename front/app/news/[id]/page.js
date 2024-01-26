0f0835879cef40d084fb7ef6aee61aa113640554
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-01-26 10:55:40 +0900

    add:news 詳細ページ終了

--- front/app/news/[id]/page.js
+++ front/app/news/[id]/page.js
@@ -1,5 +1,7 @@
 "use client";
 import { useGetNewsById } from "@/services/hooks/news";
+import React from "react";
+import { Card, CardHeader, CardBody } from "@nextui-org/card";
 
 export default function Page({params}) {
     const id = params.id;
@@ -7,6 +9,20 @@ export default function Page({params}) {
     news = news[0];
 
     return (
-    <p>{news.title}</p>
+        <Card className="py-4">
+            <CardBody className="py-2">
+                <img
+                    alt="Card background"
+                    className="overflow-hidden bg-no-repeat bg-cover w-full"
+                    height={1080}
+                    width={1920}
+                    src={news?.thumbnail_url}
+                />
+            </CardBody>
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <h1 className="font-bold text-xl">{news?.title}</h1>
+                <p className="text-tiny uppercase font-bold">{news?.text}</p>
+            </CardHeader>
+        </Card>
     )
 }
